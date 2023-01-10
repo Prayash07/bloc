e08c656a0fbcfe0898ed3764fd440d29c4c0eeac
Prayash Koirala <prayashkoirala07@gmail.com>
2023-01-10 13:50:28 +0545

    internet state

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -1,14 +1,35 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:bloc_test/constants/enums.dart';
+import 'package:bloc_test/logic/cubit/internet_cubit.dart';
 import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit()
+  final InternetCubit internetCubit;
+  late StreamSubscription internetStreamSubscription;
+
+  CounterCubit({required this.internetCubit})
       : super(CounterState(
           counterValue: 0,
           wasIncremented: false,
-        ));
+        )) {
+    monitorInternetCubit();
+  }
+
+  StreamSubscription<InternetState> monitorInternetCubit() {
+    return internetStreamSubscription = internetCubit.stream.listen((event) {
+      if (event is InternetConnected &&
+          event.connectionType == ConnectionType.wifi) {
+        increment();
+      } else if (event is InternetConnected &&
+          event.connectionType == ConnectionType.mobile) {
+        decrement();
+      }
+    });
+  }
 
   void increment() => emit(CounterState(
         counterValue: state.counterValue + 1,
@@ -18,4 +39,10 @@ class CounterCubit extends Cubit<CounterState> {
         counterValue: state.counterValue - 1,
         wasIncremented: false,
       ));
+
+  @override
+  Future<void> close() {
+    internetStreamSubscription.cancel();
+    return super.close();
+  }
 }
